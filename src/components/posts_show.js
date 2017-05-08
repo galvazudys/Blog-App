e8ee724e4e7836b731ba98c3f98022e7d512c063
galvazudys <golfas705@yahoo.com>
2017-05-08 15:43:40 +0100

    adde option to delete post with callback and option to navigate back

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchPost} from '../actions';
+import {fetchPost, deletePost} from '../actions';
+import {Link} from 'react-router-dom';
 
 class PostsShow extends Component {
 
@@ -13,6 +14,18 @@ class PostsShow extends Component {
             .fetchPost(id);
     }
 
+    onDeleteClick() {
+        const {id} = this.props.match.params;
+        this
+            .props
+            .deletePost(id, () => {
+                this
+                    .props
+                    .history
+                    .push('/');
+            });
+    }
+
     render() {
         const {post} = this.props;
 
@@ -22,6 +35,12 @@ class PostsShow extends Component {
         }
         return (
             <div>
+                <Link className="btn btn-primary" to="/">Back</Link>
+                <button
+                    className="btn btn-danger pull-xs-right"
+                    onClick={this
+                    .onDeleteClick
+                    .bind(this)}>Delete Post</button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -38,4 +57,4 @@ const mapStateToProps = ({
     };
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
